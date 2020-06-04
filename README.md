ac52586f5d354f97b36dda9814de153c2fd6b87c
Grushai Nikolay <kolyano89@yandex.ru>
2020-06-04 23:11:49 +0300

    Внесены очередные правки

--- README.md
+++ README.md
@@ -1,30 +1,8 @@
 #  https://grushainn.github.io/Newplace.github.io/
 ## v0.0.1	
-Проект собран при помощи сборщика Webpack. Все плагины установлены из онлайн библиотеки NPM. Список плагинов: 
-"devDependencies": 
-    "@babel/cli": "^7.10.1",
-    "@babel/core": "^7.10.2",
-    "@babel/preset-env": "^7.10.2",
-    "autoprefixer": "^9.8.0",
-    "babel-loader": "^8.1.0",
-    "cross-env": "^7.0.2",
-    "css-loader": "^3.5.3",
-    "cssnano": "^4.1.10",
-    "file-loader": "^6.0.0",
-    "gh-pages": "~2.0.1",
-    "html-webpack-plugin": "^4.3.0",
-    "image-webpack-loader": "^6.0.0",
-    "mini-css-extract-plugin": "^0.9.0",
-    "optimize-css-assets-webpack-plugin": "^5.0.3",
-    "postcss-loader": "^3.0.0",
-    "style-loader": "^1.2.1",
-    "webpack": "^4.43.0",
-    "webpack-cli": "^3.3.11",
-    "webpack-dev-server": "^3.11.0",
-    "webpack-md5-hash": "0.0.6"
- 
-  "dependencies": 
-    "babel-polyfill": "^6.26.0",
-    "copy-webpack-plugin": "^6.0.2",
-    "core-js": "^3.4.1" 
-Локально проект разворачивается при помощи локльного сервера и команды npm run dev поданной в Git Bash.
+###Учебный проект по использованию сборщика Webpack и взаимодействия с Github.
+
+Скачать данный проект к себе на компьютер можно пройдя по ссылке https://github.com/GrushaiNN/Newplace.github.io/tree/develop.
+Нажимаем кнопку "Clone or download" и скачиваем архив с проектом.
+Распаковываем данный архив
+Локально проект разворачивается при помощи локального сервера.
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
-const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: { main: './src/script.js' },
     output: {
@@ -35,20 +34,9 @@ module.exports = {
               test: /\.(png|jpg|gif|ico|svg)$/,
               loader: 'file-loader',
               options: {
-                  loader: 'image-webpack-loader',
                   name: '[path][name].[ext]',
                   outputPath: 'images',
                               }
-              /*use: [
-                      'file-loader?name=./src/images/[name].[ext]', // указали папку, куда складывать изображения
-                      {
-                              loader: 'image-webpack-loader',
-                              options: {
-                                name: '[name].[ext]',
-                                outputPath: 'images',
-                              }
-                      },
-              ]*/
        },
        {
         test: /\.(eot|ttf|woff|woff2)$/,
@@ -77,15 +65,7 @@ module.exports = {
 }),
   new WebpackMd5Hash(),
   new webpack.DefinePlugin({
-    'NODE_ENV': JSON.stringify('NODE_ENV')
+    'NODE_ENV': JSON.stringify('production')
 }),
-new CopyPlugin({
-  patterns: [
-      {
-          from: './src/images',
-          to: 'images'
-      }
-  ]
-})
   ]
 }
